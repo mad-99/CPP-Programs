9098a40eeefc9cb1a8151c4e9b8a0f61e70d4d98
mad-99 <72245519+mad-99@users.noreply.github.com>
2021-06-27 12:40:59 +0530

    Add files via upload

---
+++ Armstrong.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int i,sum=0,c=0,r,a,n;
+	cin>>n;
+	/*while(i%10>0)
+	{
+		c=c+1;
+		i=i/10;
+	}*/
+	r=n;
+	for(i=r;i>0;i=i/10)
+	{
+		c++;
+	}
+	while(r>0)
+	{
+		a=r%10;
+		sum=sum+pow(2,a);
+		r=r/10;
+		c--;
+	}
+	/*f(n==sum)
+	{
+		cout<<"true"<<endl;
+	}
+	else
+	{
+		cout<<"false"<<endl;
+	}*/
+	cout<<sum<<endl;
+	return 0;
+
+}
