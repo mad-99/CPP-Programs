2f4a6e67c824c8fa556f407316947f23023ed8a6
mad-99 <madhurmaheshwari1999@gmail.com>
2021-07-22 14:16:17 +0530

    main

---
+++ c.cpp
@@ -0,0 +1,28 @@
+//#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	// int no;
+	// int N;
+	// cin>>N;
+	// while(N>0)
+	// {
+	// 	cin>>no;
+	// 	int r;
+	// 	int p=1;
+	// 	int ans=0;
+	// 	while(no>0)
+	// 	{
+	// 		r=no%10;
+	// 		ans= ans+p*r;
+	// 		p=p*2;
+	// 		no=no/10;
+			
+	// 	}
+	// 	cout<<ans<<endl;
+	// 	N=N-1;
+	// }
+	printf("hello\n");
+	return 1;
+}
